39083fbb952ed32368dd18efdb652c6d74e1f587
Carolina Okamoto <cah.okamoto@hotmail.com>
2013-03-21 15:55:00 -0300

    Primeira classe basica iniciada (DVD)

---
+++ nbproj/src/br/unesp/igce/SGT/DVD.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.unesp.igce.SGT;
+
+/**
+ *
+ * @author aluno
+ */
+public class DVD {
+    private String titulo;
+    private String diretor;
+    
+    DVD (String titulo) {
+        this.titulo = titulo;
+        this.diretor = " ";
+        
+    }
+    
+    public static void main (String[] args) {
+        DVD x = new DVD ("Django Livre");
+    }
+    
+}
